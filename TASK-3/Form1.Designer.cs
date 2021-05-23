1119ccbe9403b941b9d822b031cd330d8d0147dd
Vlada Novikova <42334785+velencie@users.noreply.github.com>
2021-05-24 00:01:58 +0300

    Add TASK-3

---
+++ TASK-3/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace WindowsFormsApp1
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.label1 = new System.Windows.Forms.Label();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.textBox2 = new System.Windows.Forms.TextBox();
+      this.button1 = new System.Windows.Forms.Button();
+      this.groupBox1 = new System.Windows.Forms.GroupBox();
+      this.radioButton1 = new System.Windows.Forms.RadioButton();
+      this.radioButton2 = new System.Windows.Forms.RadioButton();
+      this.radioButton3 = new System.Windows.Forms.RadioButton();
+      this.groupBox1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(73, 98);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(77, 17);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "Введите x:";
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(171, 95);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(151, 22);
+      this.textBox1.TabIndex = 1;
+      // 
+      // textBox2
+      // 
+      this.textBox2.Location = new System.Drawing.Point(76, 172);
+      this.textBox2.Multiline = true;
+      this.textBox2.Name = "textBox2";
+      this.textBox2.Size = new System.Drawing.Size(246, 190);
+      this.textBox2.TabIndex = 2;
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(390, 314);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(95, 35);
+      this.button1.TabIndex = 4;
+      this.button1.Text = "Расчёт";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // groupBox1
+      // 
+      this.groupBox1.Controls.Add(this.radioButton3);
+      this.groupBox1.Controls.Add(this.radioButton2);
+      this.groupBox1.Controls.Add(this.radioButton1);
+      this.groupBox1.Location = new System.Drawing.Point(369, 95);
+      this.groupBox1.Name = "groupBox1";
+      this.groupBox1.Size = new System.Drawing.Size(127, 112);
+      this.groupBox1.TabIndex = 5;
+      this.groupBox1.TabStop = false;
+      this.groupBox1.Text = "F(x)";
+      // 
+      // radioButton1
+      // 
+      this.radioButton1.AutoSize = true;
+      this.radioButton1.Checked = true;
+      this.radioButton1.Location = new System.Drawing.Point(7, 22);
+      this.radioButton1.Name = "radioButton1";
+      this.radioButton1.Size = new System.Drawing.Size(44, 21);
+      this.radioButton1.TabIndex = 0;
+      this.radioButton1.TabStop = true;
+      this.radioButton1.Text = "sh";
+      this.radioButton1.UseVisualStyleBackColor = true;
+      // 
+      // radioButton2
+      // 
+      this.radioButton2.AutoSize = true;
+      this.radioButton2.Location = new System.Drawing.Point(7, 50);
+      this.radioButton2.Name = "radioButton2";
+      this.radioButton2.Size = new System.Drawing.Size(44, 21);
+      this.radioButton2.TabIndex = 1;
+      this.radioButton2.Text = "ch";
+      this.radioButton2.UseVisualStyleBackColor = true;
+      // 
+      // radioButton3
+      // 
+      this.radioButton3.AutoSize = true;
+      this.radioButton3.Location = new System.Drawing.Point(6, 77);
+      this.radioButton3.Name = "radioButton3";
+      this.radioButton3.Size = new System.Drawing.Size(51, 21);
+      this.radioButton3.TabIndex = 2;
+      this.radioButton3.Text = "exp";
+      this.radioButton3.UseVisualStyleBackColor = true;
+      this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(604, 450);
+      this.Controls.Add(this.groupBox1);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.textBox2);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.label1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.groupBox1.ResumeLayout(false);
+      this.groupBox1.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.TextBox textBox2;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.GroupBox groupBox1;
+    private System.Windows.Forms.RadioButton radioButton3;
+    private System.Windows.Forms.RadioButton radioButton2;
+    private System.Windows.Forms.RadioButton radioButton1;
+  }
+}
+
---
+++ TASK-3/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+      private void button1_Click(object sender, EventArgs e)
+    {
+      double x = Convert.ToDouble(textBox1.Text);
+      textBox2.Text = "Результаты работы программы ст. Новиковой В.А. " + Environment.NewLine;
+
+      int n = 0;
+      if (radioButton2.Checked) n = 1;
+      else if (radioButton3.Checked) n = 2;
+
+      double u;
+      switch (n)
+      {
+        case 0:
+          if (x > 5) u = 0.5 * (Math.Exp(x) - Math.Exp(-x)) / 2;
+          else if (x >= 0 && x <= 3) u = 2;
+          else u = 1/((Math.Exp(x) - Math.Exp(-x)) / 2);
+          textBox2.Text += "U = " + Convert.ToString(u) + Environment.NewLine;
+          break;
+        case 1:
+          if (x > 5) u = 0.5 * (Math.Exp(x) + Math.Exp(-x)) / 2;
+          else if (x >= 0 && x <= 3) u = 2;
+          else u = 1 /((Math.Exp(x) + Math.Exp(-x)) / 2);
+          textBox2.Text += "U = " + Convert.ToString(u) + Environment.NewLine;
+          break;
+        case 2:
+          if (x > 5) u = 0.5 * Math.Exp(x);
+          else if (x >= 0 && x <= 3) u = 2;
+          else u = 1 / (Math.Exp(x));
+          textBox2.Text += "U = " + Convert.ToString(u) + Environment.NewLine;
+          break;
+        default:
+          textBox2.Text += "Решение не найдено" + Environment.NewLine;
+          break;
+      }
+    }
+
+    private void radioButton3_CheckedChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
