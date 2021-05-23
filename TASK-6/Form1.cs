7ae308d91d8503f8fe49dd598fb4aaa46339f3ad
Vlada Novikova <42334785+velencie@users.noreply.github.com>
2021-05-24 01:08:11 +0300

    Add task 6

---
+++ TASK-6/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+  public partial class Form1 : Form
+  {
+    int[] Mas = new int[15];
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      Random rand = new Random();
+      textBox1.Text = "";
+      for (int i = 0; i < 15; i++)
+      {
+        Mas[i] = rand.Next(-50, 50);
+        textBox1.Text += "Mas[" + Convert.ToString(i) + "] = "
+        + Convert.ToString(Mas[i]) + Environment.NewLine;
+      }
+
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      textBox2.Text = "";
+      for (int i = 0; i < 15; i++)
+      {
+        if (Mas[i] < 0) Mas[i] = 0;
+        textBox2.Text += "Mas[" + Convert.ToString(i) + "] = "
+        + Convert.ToString(Mas[i]) + Environment.NewLine;
+      }
+    }
+  }
+}
