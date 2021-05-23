1ddf6296417797e30af91385cf9f09fa0a74ee12
Vlada Novikova <42334785+velencie@users.noreply.github.com>
2021-05-24 00:24:00 +0300

    Add TASK-4

---
+++ TASK-4/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace WindowsFormsApp1
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.button1 = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.radioButton1 = new System.Windows.Forms.RadioButton();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.textBox2 = new System.Windows.Forms.TextBox();
+      this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(447, 312);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(105, 32);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "Расчёт";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(89, 78);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(127, 17);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "Введите точность";
+      // 
+      // radioButton1
+      // 
+      this.radioButton1.AutoSize = true;
+      this.radioButton1.Checked = true;
+      this.radioButton1.Location = new System.Drawing.Point(447, 158);
+      this.radioButton1.Name = "radioButton1";
+      this.radioButton1.Size = new System.Drawing.Size(107, 21);
+      this.radioButton1.TabIndex = 2;
+      this.radioButton1.TabStop = true;
+      this.radioButton1.Text = "Сумма ряда";
+      this.radioButton1.UseVisualStyleBackColor = true;
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(241, 75);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(100, 22);
+      this.textBox1.TabIndex = 3;
+      // 
+      // textBox2
+      // 
+      this.textBox2.Location = new System.Drawing.Point(37, 143);
+      this.textBox2.Multiline = true;
+      this.textBox2.Name = "textBox2";
+      this.textBox2.Size = new System.Drawing.Size(377, 222);
+      this.textBox2.TabIndex = 4;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(609, 450);
+      this.Controls.Add(this.textBox2);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.radioButton1);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.button1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.RadioButton radioButton1;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.TextBox textBox2;
+  }
+}
+
---
+++ TASK-4/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+
+    public static long Fact(long a)
+    {
+      if (a == 0)
+        return 1;
+      else
+        return a * Fact(a - 1);
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      Double n = Convert.ToDouble(textBox1.Text);
+      Double eps = Convert.ToDouble(textBox1.Text);
+      textBox2.Text = "Результаты работы программы ст. Новиковой В.А. " + Environment.NewLine;
+
+      double s = 0, ch;
+      long i = 1;
+
+      ch = 2 / Math.Sqrt(Fact(i) + 4);
+
+      while (ch >= eps)
+      {
+        ch = 2 / Math.Sqrt(Fact(i) + 4);
+        s += ch;
+        i++;
+      }
+      textBox2.Text += "При eps = " + textBox1.Text + Environment.NewLine;
+      textBox2.Text += "Расчет суммы ряда S = " + Convert.ToString(s) + Environment.NewLine;
+    }
+  }
+}
