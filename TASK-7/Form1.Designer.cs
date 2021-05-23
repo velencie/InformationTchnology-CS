4333b73b65ad81abdf47733c3b686fbecfa1249e
Vlada Novikova <42334785+velencie@users.noreply.github.com>
2021-05-24 02:04:30 +0300

    Add task 7

---
+++ TASK-7/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace WindowsFormsApp4
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.dataGridView1 = new System.Windows.Forms.DataGridView();
+      this.label1 = new System.Windows.Forms.Label();
+      this.button1 = new System.Windows.Forms.Button();
+      this.dataGridView2 = new System.Windows.Forms.DataGridView();
+      this.label2 = new System.Windows.Forms.Label();
+      this.label3 = new System.Windows.Forms.Label();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // dataGridView1
+      // 
+      this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dataGridView1.Location = new System.Drawing.Point(12, 60);
+      this.dataGridView1.Name = "dataGridView1";
+      this.dataGridView1.RowHeadersWidth = 51;
+      this.dataGridView1.RowTemplate.Height = 24;
+      this.dataGridView1.Size = new System.Drawing.Size(565, 527);
+      this.dataGridView1.TabIndex = 0;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(227, 26);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(131, 17);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "Исходная матрица";
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(12, 609);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(172, 32);
+      this.button1.TabIndex = 2;
+      this.button1.Text = "Заполнение массива";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // dataGridView2
+      // 
+      this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dataGridView2.Location = new System.Drawing.Point(645, 60);
+      this.dataGridView2.Name = "dataGridView2";
+      this.dataGridView2.RowHeadersWidth = 51;
+      this.dataGridView2.RowTemplate.Height = 24;
+      this.dataGridView2.Size = new System.Drawing.Size(575, 527);
+      this.dataGridView2.TabIndex = 4;
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(857, 26);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(149, 17);
+      this.label2.TabIndex = 5;
+      this.label2.Text = "Полученная матрица";
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(642, 609);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(46, 17);
+      this.label3.TabIndex = 6;
+      this.label3.Text = "label3";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1231, 657);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.dataGridView2);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.dataGridView1);
+      this.Name = "Form1";
+      this.Text = "Двумерные массивы";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.DataGridView dataGridView1;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.DataGridView dataGridView2;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label label3;
+  }
+}
+
---
+++ TASK-7/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      dataGridView1.RowCount = 15; 
+      dataGridView1.ColumnCount = 15;
+
+      dataGridView2.RowCount = 15;
+      dataGridView2.ColumnCount = 15;
+
+      int[,] a = new int[15, 15]; 
+      int i, j;
+      Random rand = new Random();
+      for (i = 0; i < 15; i++)
+        for (j = 0; j < 15; j++)
+          a[i, j] = rand.Next(-100, 100);
+      for (i = 0; i < 15; i++)
+        for (j = 0; j < 15; j++)
+        {
+          dataGridView1.Rows[i].Cells[j].Value = Convert.ToString(a[i, j]);
+          dataGridView2.Rows[i].Cells[j].Value = Convert.ToString(a[i, j]);
+        }  
+          
+      int max_elem = int.MinValue;
+      int min_elem = int.MaxValue;
+      int str_min = 0, str_max = 0;
+
+      for (i = 0; i < 15; i++)
+        for (j = 0; j < 15; j++)
+          if (a[i, j] > max_elem)
+          {
+            max_elem = a[i, j];
+            str_max = i;
+          };
+
+      for (i = 0; i < 15; i++)
+        for (j = 0; j < 15; j++)
+          if (a[i, j] < min_elem)
+          {
+            min_elem = a[i, j];
+            str_min = i;
+          }
+
+
+      if (str_min > str_max) str_min = str_max + (str_max = str_min) * 0;
+
+      DataGridViewRow row1 = dataGridView2.Rows[str_min];
+      DataGridViewRow row2 = dataGridView2.Rows[str_max];
+
+      dataGridView2.Rows.Remove(row1);
+      dataGridView2.Rows.Remove(row2);
+
+      dataGridView2.Rows.Insert(str_min, row2);
+      dataGridView2.Rows.Insert(str_max, row1);
+
+      int k_max = str_max + 1;
+      int k_min = str_min + 1;
+
+      label3.Text = "Поменяны строки местами " + k_max.ToString() + " и " + k_min.ToString();
+
+    }
+  }
+}
